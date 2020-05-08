7b7d4fe09872ac4294ba40d605bc9d2e897c8b2c
FF1204 <49927705+FF1204@users.noreply.github.com>
2020-05-08 16:58:49 +0800

    Update plugin.py

--- mkdocs_render_otherfile_plugin/plugin.py
+++ mkdocs_render_otherfile_plugin/plugin.py
@@ -22,7 +22,21 @@ class RenderOtherfilePlugin(BasePlugin):
         listext = self.config['ext']
         utils.markdown_extensions.extend(listext)
         return
-    
+    def on_files(self, files, config):
+        for file in files._files:
+            src_path = file.src_path
+            filename, ext = os.path.splitext(src_path)
+            listext = self.config['ext']
+            if ext in listext:
+                dest_path, html1 = os.path.splitext(file.dest_path)
+                abs_dest_path, html2 = os.path.splitext(file.abs_dest_path)
+
+                file.dest_path = dest_path + ext + html1
+                file.abs_dest_path = abs_dest_path + ext + html2
+                file.url = dest_path.replace("\\", '/') + ext + html1
+                file.name = file.name + ext
+
+        return files 
     def on_page_markdown(self, markdown, page, config, files):
         """
         The page_markdown event is called after the page's markdown is loaded 
@@ -46,8 +60,9 @@ class RenderOtherfilePlugin(BasePlugin):
         src_file_path = page.file.abs_src_path
         prepath, ext = os.path.splitext(src_file_path)
         lang = ext.lstrip('.')
+        filename = page.file.name
         if ext in listext:
-            new_markdown = "```{0}\n".format(lang) + markdown + "\n```"
+            new_markdown = "# {0}\n\n```{1}\n".format(filename, lang) + markdown + "\n```"
             return new_markdown
         else:
             return markdown
